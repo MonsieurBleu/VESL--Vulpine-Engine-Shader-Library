d6da48e7a553d600f08c4cd19a3444093fc39677
ClairDeLuna01 <luna.bossu@gmail.com>
2025-01-05 20:20:01 +0100

    fixed multiple bugs with the skybox

--- .include/functions/Skybox.glsl
+++ .include/functions/Skybox.glsl
@@ -30,156 +30,9 @@ vec3 getStars(vec3 viewDir, float starSize, float starDensity, float maxSaturati
     return col * starFactor;
 }
 
-
-#define IN_STEPS 64
-// at least 2 steps (or we get very wrong results) but 3 is better
-#define OUT_STEPS 2
-
-
-// Constants from https://www.shadertoy.com/view/wlBXWK
-#define PLANET_RADIUS 6371e3 /* radius of the planet */
-#define ATMOS_RADIUS 6471e3 /* radius of the atmosphere */
-
-#define RAY_BETA vec3(5.5e-6, 13.0e-6, 22.4e-6) /* rayleigh, affects the color of the sky */
-#define MIE_BETA vec3(21e-6) /* mie, affects the color of the blob around the sun */
-#define AMBIENT_BETA vec3(0.0) /* ambient, affects the scattering color when there is no lighting from the sun */
-#define ABSORPTION_BETA vec3(2.04e-5, 4.97e-5, 1.95e-6) /* what color gets absorbed by the atmosphere (Due to things like ozone) */
-#define G 0.995 /* mie scattering direction, or how big the blob around the sun is */
-
-#define HEIGHT_RAY 8e3 /* rayleigh height */
-#define HEIGHT_MIE 0.3e3 /* and mie */
-#define HEIGHT_ABSORPTION 30e3 /* at what height the absorption is at it's maximum */
-#define ABSORPTION_FALLOFF 8e3 /* how much the absorption decreases the further away it gets from the maximum height */
-
-
-
-// heavily inspired by https://www.shadertoy.com/view/wlBXWK
-vec3 atmosphericScattering(
-    vec3 origin,
-    vec3 viewDir,
-    vec3 lightDir,
-    vec3 sunIntensity,
-    vec3 sceneColor,
-    float maxDist,
-    out float brightness
-)
-{
-    float a = dot(viewDir, viewDir);
-    float b = 2.0 * dot(viewDir, origin);
-    float c = dot(origin, origin) - (ATMOS_RADIUS * ATMOS_RADIUS);
-    float d = (b * b) - 4.0 * a * c;
-
-    if (d < 0.0) return sceneColor;
-
-    vec2 rayLength = vec2(
-        max((-b - sqrt(d)) / (2.0 * a), 0.0),
-        min((-b + sqrt(d)) / (2.0 * a), maxDist)
-    );
-
-    if (rayLength.x > rayLength.y) return sceneColor;
-
-    
-
-    float stepSizeIn = (rayLength.y - rayLength.x) / float(IN_STEPS);
-
-    float rayPosIn = rayLength.x + stepSizeIn * 0.5;
-
-    vec3 totalRay = vec3(0.0);
-    vec3 totalMie = vec3(0.0);
-
-    vec3 opticalDepthIn = vec3(0.0);
-
-    vec2 scaleHeight = vec2(HEIGHT_RAY, HEIGHT_MIE);
-
-    float mu = dot(viewDir, lightDir);
-    float mumu = mu * mu;
-    float gg = G * G;
-    float phaseRay = 3.0 / (50.2654824574 /* (16 * pi) */) * (1.0 + mumu);
-    float phaseMie = 3.0 / (25.1327412287 /* (8 * pi) */) * ((1.0 - gg) * (mumu + 1.0)) / (pow(1.0 + gg - 2.0 * mu * G, 1.5) * (2.0 + gg));
-
-
-    for (int i = 0; i < IN_STEPS; i++)
-    {
-        vec3 p = origin + viewDir * viewDir * rayPosIn;
-        float h = length(p) - PLANET_RADIUS;
-
-        vec3 density = vec3(exp(-h / scaleHeight), 0.0);
-
-        float denom = (HEIGHT_ABSORPTION - h) / ABSORPTION_FALLOFF;
-        density.z = (1.0 / (denom * denom + 1.0)) * density.x;
-
-        density *= stepSizeIn;
-
-        opticalDepthIn += density;
-
-        float a = dot(lightDir, lightDir);
-        float b = 2.0 * dot(lightDir, p);
-        float c = dot(p, p) - (ATMOS_RADIUS * ATMOS_RADIUS);
-        float d = (b * b) - 4.0 * a * c;
-
-        float stepSizeOut = (-b + sqrt(d)) / (2.0 * a * float(OUT_STEPS));
-
-        float rayPosOut = stepSizeOut * 0.5;
-
-        vec3 opticalDepthOut = vec3(0.0);
-        for (int j = 0; j < OUT_STEPS; j++)
-        {
-            vec3 pOut = p + lightDir * rayPosOut;
-            float hOut = length(pOut) - PLANET_RADIUS;
-            
-            vec3 densityOut = vec3(exp(-hOut / scaleHeight), 0.0);
-            float denomOut = (HEIGHT_ABSORPTION - hOut) / ABSORPTION_FALLOFF;
-            densityOut.z = (1.0 / (denomOut * denomOut + 1.0)) * densityOut.x;
-
-            densityOut *= stepSizeOut;
-
-            opticalDepthOut += densityOut;
-
-            rayPosOut += stepSizeOut;
-        }
-        
-        vec3 attenuation = exp(-RAY_BETA * (opticalDepthIn.x + opticalDepthOut.x) - 
-                                MIE_BETA * (opticalDepthIn.y + opticalDepthOut.y) - 
-                                ABSORPTION_BETA * (opticalDepthIn.z + opticalDepthOut.z));
-        
-        totalRay += max(density.x * attenuation, 0.0);
-        totalMie += max(density.y * attenuation, 0.0);
-
-        rayPosIn += stepSizeIn;
-    }
-
-    vec3 opacity = exp(-(MIE_BETA * opticalDepthIn.y + 
-                         RAY_BETA * opticalDepthIn.x + 
-                         ABSORPTION_BETA * opticalDepthIn.z));
-
-    vec3 scatteredLight = (
-        phaseRay * RAY_BETA * totalRay + 
-        phaseMie * MIE_BETA * totalMie + 
-        opticalDepthIn.x * AMBIENT_BETA
-        );
-
-    brightness = dot(scatteredLight, vec3(0.2126, 0.7152, 0.0722)); 
-
-    return scatteredLight * sunIntensity + sceneColor * opacity;
-}
-
-vec3 rotate(vec3 v, float a, vec3 axis)
-{
-    float s = sin(a);
-    float c = cos(a);
-    float oc = 1.0 - c;
-
-    mat3 rot = mat3(
-        oc * axis.x * axis.x + c, oc * axis.x * axis.y - axis.z * s, oc * axis.z * axis.x + axis.y * s,
-        oc * axis.x * axis.y + axis.z * s, oc * axis.y * axis.y + c, oc * axis.y * axis.z - axis.x * s,
-        oc * axis.z * axis.x - axis.y * s, oc * axis.y * axis.z + axis.x * s, oc * axis.z * axis.z + c
-    );
-
-    return rot * v;
-}
-
 struct rayTraceOut {
     float t;
+    float t2;
     vec3 intersectPoint;
     vec3 normal;
     vec2 uv;
@@ -212,6 +65,7 @@ rayTraceOut sphereIntersect(vec3 origin, vec3 direction, vec3 center, float radi
     }
 
     float t;
+    float t_max = max(t1, t2);
     
     if (t1 < 0.0001)
     {
@@ -228,6 +82,7 @@ rayTraceOut sphereIntersect(vec3 origin, vec3 direction, vec3 center, float radi
 
     rayTraceOut rslt;
     rslt.t = t;
+    rslt.t2 = t_max;
     rslt.intersectPoint = origin + direction * t;
     rslt.normal = normalize(rslt.intersectPoint - center);
     
@@ -241,6 +96,190 @@ rayTraceOut sphereIntersect(vec3 origin, vec3 direction, vec3 center, float radi
     return rslt;
     // return origin + direction * t;
 }
+
+
+#define IN_STEPS 64
+// at least 2 steps (or we get very wrong results) but 3 is better
+#define OUT_STEPS 3
+
+
+// Constants from https://www.shadertoy.com/view/wlBXWK
+#define PLANET_RADIUS 6371e3 /* radius of the planet */
+#define ATMOS_RADIUS 6471e3 /* radius of the atmosphere */
+
+#define RAY_BETA vec3(5.5e-6, 13.0e-6, 22.4e-6) /* rayleigh, affects the color of the sky */
+#define MIE_BETA vec3(21e-6) /* mie, affects the color of the blob around the sun */
+#define AMBIENT_BETA vec3(0.0) /* ambient, affects the scattering color when there is no lighting from the sun */
+#define ABSORPTION_BETA vec3(2.04e-5, 4.97e-5, 1.95e-6) /* what color gets absorbed by the atmosphere (Due to things like ozone) */
+#define G 0.995 /* mie scattering direction, or how big the blob around the sun is */
+
+#define HEIGHT_RAY 8e3 /* rayleigh height */
+#define HEIGHT_MIE 0.3e3 /* and mie */
+#define HEIGHT_ABSORPTION 30e3 /* at what height the absorption is at it's maximum */
+#define ABSORPTION_FALLOFF 8e3 /* how much the absorption decreases the further away it gets from the maximum height */
+
+
+
+// heavily inspired by https://www.shadertoy.com/view/wlBXWK
+vec3 atmosphericScattering(
+    vec3 origin,
+    vec3 viewDir,
+    vec3 lightDir,
+    vec3 sunIntensity,
+    vec3 sceneColor,
+    float maxDist,
+    out float brightness
+)
+{   
+    // rayTraceOut planetIntersect = sphereIntersect(origin, viewDir, vec3(0), PLANET_RADIUS);
+
+    // if (planetIntersect.t > 0.0)
+    // {
+    //     // move the origin to the far side of the planet
+    //     vec3 farSide = origin + viewDir * planetIntersect.t2;
+    //     origin = farSide + viewDir * 1000.0;
+    // }
+
+    float a = dot(viewDir, viewDir);
+    float b = 2.0 * dot(viewDir, origin);
+    float c = dot(origin, origin) - (ATMOS_RADIUS * ATMOS_RADIUS);
+    float d = (b * b) - 4.0 * a * c + 1e-6;
+
+    // if (d < 0.0) return sceneColor;
+
+    vec2 rayLength = vec2(
+        max((-b - sqrt(d)) / (2.0 * a), 0.0),
+        min((-b + sqrt(d)) / (2.0 * a), maxDist)
+    );
+
+
+
+
+    // if (rayLength.x > rayLength.y) return sceneColor;
+
+    
+
+    float stepSizeIn = (rayLength.y - rayLength.x) / float(IN_STEPS);
+
+    float rayPosIn = rayLength.x + stepSizeIn;
+
+    vec3 totalRay = vec3(0.0);
+    vec3 totalMie = vec3(0.0);
+
+    vec3 opticalDepthIn = vec3(0.0);
+
+    vec2 scaleHeight = vec2(HEIGHT_RAY, HEIGHT_MIE);
+
+    float mu = dot(viewDir, lightDir);
+    float mumu = mu * mu;
+    float gg = G * G;
+    float phaseRay = 3.0 / (50.2654824574 /* (16 * pi) */) * (1.0 + mumu);
+    float phaseMie = 3.0 / (25.1327412287 /* (8 * pi) */) * ((1.0 - gg) * (mumu + 1.0)) / (pow(1.0 + gg - 2.0 * mu * G, 1.5) * (2.0 + gg));
+
+    // return vec3(phaseMie);
+
+    for (int i = 0; i < IN_STEPS; i++)
+    {
+        vec3 p = origin + viewDir * rayPosIn;
+        float h = length(p) - PLANET_RADIUS;
+
+        vec3 density = vec3(exp(-h / scaleHeight), 0.0);
+
+        float denom = (HEIGHT_ABSORPTION - h) / ABSORPTION_FALLOFF;
+        density.z = (1.0 / (denom * denom + 1.0)) * density.x;
+
+        density *= stepSizeIn;
+
+        opticalDepthIn += density;
+
+        float a = dot(lightDir, lightDir);
+        float b = 2.0 * dot(lightDir, p);
+        float c = dot(p, p) - (ATMOS_RADIUS * ATMOS_RADIUS);
+        float d = (b * b) - 4.0 * a * c + 1e-6;
+
+        float stepSizeOut = (-b + sqrt(d)) / (2.0 * a * float(OUT_STEPS));
+
+        float rayPosOut = stepSizeOut;
+
+        vec3 opticalDepthOut = vec3(0.0);
+        int validOut = 0;
+        for (int j = 0; j < OUT_STEPS; j++)
+        {
+            vec3 pOut = p + lightDir * rayPosOut;
+            float hOut = length(pOut) - PLANET_RADIUS;
+
+            // if (hOut < 0.0) break;
+        
+            vec3 densityOut = vec3(exp(-hOut / scaleHeight), 0.0);
+
+            float denomOut = (HEIGHT_ABSORPTION - hOut) / ABSORPTION_FALLOFF;
+            densityOut.z = (1.0 / (denomOut * denomOut + 1.0)) * densityOut.x;
+
+            // return vec3(hOut);
+
+            opticalDepthOut += densityOut;
+
+            validOut++;
+
+            rayPosOut += stepSizeOut;
+        }
+
+        if (validOut == 0) continue;
+
+        opticalDepthOut *= stepSizeOut;
+        
+        vec3 attenuation = exp(-
+            RAY_BETA        * (opticalDepthIn.x + opticalDepthOut.x) - 
+            MIE_BETA        * (opticalDepthIn.y + opticalDepthOut.y) - 
+            ABSORPTION_BETA * (opticalDepthIn.z + opticalDepthOut.z)
+        );
+        // vec3 attenuation = exp(-
+        //     RAY_BETA        * opticalDepthIn.x - 
+        //     MIE_BETA        * opticalDepthIn.y - 
+        //     ABSORPTION_BETA * opticalDepthIn.z
+        // );
+
+        // return vec3(opticalDepthOut.x / 1000000.0);
+        
+        totalRay += max(density.x * attenuation, 0.0);
+        totalMie += max(density.y * attenuation, 0.0);
+
+        rayPosIn += stepSizeIn;
+    }
+
+    vec3 opacity = exp(-
+        MIE_BETA        * opticalDepthIn.y - 
+        RAY_BETA        * opticalDepthIn.x - 
+        ABSORPTION_BETA * opticalDepthIn.z
+    );
+
+    vec3 scatteredLight = (
+        phaseRay * RAY_BETA * totalRay + 
+        phaseMie * MIE_BETA * totalMie + 
+        opticalDepthIn.x * AMBIENT_BETA
+        );
+
+    brightness = dot(scatteredLight, vec3(0.2126, 0.7152, 0.0722)); 
+
+    return scatteredLight * sunIntensity + sceneColor * opacity;
+}
+
+vec3 rotate(vec3 v, float a, vec3 axis)
+{
+    float s = sin(a);
+    float c = cos(a);
+    float oc = 1.0 - c;
+
+    mat3 rot = mat3(
+        oc * axis.x * axis.x + c, oc * axis.x * axis.y - axis.z * s, oc * axis.z * axis.x + axis.y * s,
+        oc * axis.x * axis.y + axis.z * s, oc * axis.y * axis.y + c, oc * axis.y * axis.z - axis.x * s,
+        oc * axis.z * axis.x - axis.y * s, oc * axis.y * axis.z + axis.x * s, oc * axis.z * axis.z + c
+    );
+
+    return rot * v;
+}
+
+
 
 #define MOON_RADIUS 1.0*12737.1e3
 #define MOON_DISTANCE 384400e3
@@ -279,9 +318,8 @@ void getMoonColor(vec3 origin, vec3 direction, inout vec3 color, float opacity)
         // colMoon = vec3(moonIntersect.uv, 1.0);
 
         vec3 moonColor = mix(colorBlack, colMoon, moonIntensity)*vec3(1.0, 0.9, 0.8)*1.5;
-        color = mix(color, moonColor, max(opacity, 0.15));
+        color = mix(color, moonColor, max(opacity, 0.35 * moonIntensity));
     }
-
 }
 
 void getSkyColors(
