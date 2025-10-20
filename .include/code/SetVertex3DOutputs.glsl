1cc59ddf22261518c08e94c13ea7484933aad441
MonsieurBleu <97893210+MonsieurBleu@users.noreply.github.com>
2025-10-20 16:20:22 +0200

    Update SetVertex3DOutputs.glsl

--- .include/code/SetVertex3DOutputs.glsl
+++ .include/code/SetVertex3DOutputs.glsl
@@ -13,7 +13,7 @@ normal = normalize(modelMatrix * vec4(normal, 0.0)).rgb;
 position = (modelMatrix * vec4(positionInModel, 1.0)).rgb;
 viewVector = _cameraPosition - position;
 
-// #define DO_FAKE_PLANET_CURVATURE
+#define DO_FAKE_PLANET_CURVATURE
 #ifdef DO_FAKE_PLANET_CURVATURE
 {
     #define BASE_SIZE 8192 * 2.
